dc3d5c4fb95147136c5e0e16de1c3f8eb7d08a3f
Francisco Dias <fdias@opera.com>
2026-01-19 18:51:11 +0000

    Added support for uint64

--- ExtensionGenerator/extgen/Parsing/Gmidl/GmidlSchemaParser.cs
+++ ExtensionGenerator/extgen/Parsing/Gmidl/GmidlSchemaParser.cs
@@ -194,6 +194,9 @@ namespace extgen.Parsing.Gmidl
 
             IrType ResolveFromHint(string hint, bool isOpt, bool isColl, int? len)
             {
+                if (hint.Equals("uint64", StringComparison.OrdinalIgnoreCase))
+                    return IrType.UInt64 with { IsNullable = isOpt, IsCollection = isColl, FixedLength = len };
+
                 // builtin keywords that are not in GMIDLPrimitive mapping, if any:
                 if (hint.Equals("buffer", StringComparison.OrdinalIgnoreCase))
                     return IrType.Buffer with { IsNullable = isOpt, IsCollection = isColl, FixedLength = len };
